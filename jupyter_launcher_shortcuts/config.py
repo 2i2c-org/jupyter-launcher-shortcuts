37cee41155cace33de6fd1fe8e3369d5a80be693
Erik Sundell <erik.i.sundell@gmail.com>
2024-07-08 09:39:15 +0200

    Cleanup unreferenced copy-paste remnant file

--- jupyter_launcher_shortcuts/config.py
+++
@@ -1,8 +0,0 @@
-"""
-Traitlets based configuration for jupyter_server_proxy
-"""
-from jupyter_server.utils import url_path_join as ujoin
-from .handlers import SuperviseAndProxyHandler, AddSlashHandler
-import pkg_resources
-from collections import namedtuple
-
